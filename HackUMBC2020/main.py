5898329b18094fbca6cd185742961e551ee00d6f
Jaspr2020 <jsr5577@gmail.com>
2020-11-14 17:11:42 -0500

    Rolling dice

--- HackUMBC2020/main.py
+++ HackUMBC2020/main.py
@@ -1,11 +1,27 @@
-# This is a sample Python script.
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def rollDice(numRolls):
+    rolls = []
 
-# Press the green button in the gutter to run the script.
+    for i in range(numRolls):
+        rolls.append(random.randrange(1, 6))
+
+    return rolls
+
+def printRolls(rolls):
+    for roll in rolls:
+        print("You rolled " + str(roll))
+
 if __name__ == '__main__':
-    print("We will win")
-    print("nah")
+    numRolls = -1
+    while numRolls < 1:
+        numRolls = input("How many rolls? ")
+        if not numRolls.isnumeric():
+            numRolls = -1
+        else:
+            numRolls = int(numRolls)
+
+    rolls = rollDice(3)
+    printRolls(rolls)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
