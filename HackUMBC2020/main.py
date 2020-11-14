943d5162ae67ff842cdb71c10b0ec8d0b032c54e
Sparrow-Redsage <vham2952@gmail.com>
2020-11-14 16:36:27 -0500

    No thanks

--- HackUMBC2020/main.py
+++ HackUMBC2020/main.py
@@ -6,5 +6,6 @@
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print("We will win")
+    print("nah")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
